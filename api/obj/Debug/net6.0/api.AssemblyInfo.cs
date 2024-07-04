3beaac129dcfda9288d75d7660c7efa8c066c63e
dhvietanh1001 <vieanh02@gmail.com>
2024-07-04 20:57:50 +0700

    2 lan

--- api/obj/Debug/net6.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net6.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7cf8a173811107f4828bcac82edb34a74dbd2ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
